edaf2d973ac33c39f83c9f6237b421b2642af266
viktoravtomat <viktoravtomat@gmail.com>
2021-11-24 14:32:54 +0300

    Начальная форма

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sqlite3
 import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtWidgets import QTableWidgetItem
 
 
